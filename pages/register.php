8c04608f34b509518475ec3739062cef8fcad436
AlexanderTse123 <46985670+AlexanderTse123@users.noreply.github.com>
2021-03-12 21:51:16 -0500

    Pages done by Alex

---
+++ pages/register.php
@@ -0,0 +1,11 @@
+Register
+<form method="POST" action="gamepage.php">
+	<label for="username">Username:</label>
+	<input type="username" id="username" name="username" required/>
+	<label for="password">Password:</label>
+	<input type="password" id="password" name="password" required/>
+	<label for="confirmation">Confirm Password:</label>
+	<input type="password" id="confirmation" name="confirm" required/>
+	<input type="submit" name="register" value="Register"/>
+	<a href="home.php">Already have an Account? Click here"</a>
+</form>
