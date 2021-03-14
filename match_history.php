5a5ec6e275c142f16bb7c208c33bb35d60378aa0
jja42 <47000512+jja42@users.noreply.github.com>
2021-03-13 21:11:21 -0500

    Update match_history.php

--- match_history.php
+++ match_history.php
@@ -1,5 +1,5 @@
 <?php
-$winner = (int)$_POST["winner"];
+$winner = $_POST["winner"];
 $loser = $_POST["loser"];
 $output = shell_exec("php rabbitmqphp_example/Match_History.php $winner $loser");
 echo $output;	
