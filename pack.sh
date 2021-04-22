c6d148116b3a5ce16b1656f1a555c3972d37fce3
jja42 <jja2@njit.edu>
2021-04-22 14:38:23 -0400

    Modified Packing for Version Number and Package Name

--- pack.sh
+++ pack.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-version_num="test_version"
-package_path="packages/$version_num"
-mkdir -p $package_path
-
-for i; do
-	cp -r $i $package_path
+package_name=$1
+version_num=$2
+package_path="packages/$package_name/$version_num"
+mkdir $package_path
+argc=$#
+argv=("$@")
+for ((i=2;i<$argc;i++)); do
+	cp -r ${argv[$i]} $package_path
 done
 
-package='testPackage_version_'$version_num.tgz
+package=$package_name'_'$version_num.tgz
 tar -zcvf $package_path'/'$package $package_path
 
-scp $package_path'/'$package joao-dev@25.89.104.232:~/Downloads
-
-#rm -R $package_path
+#scp $package_path'/'$package joao-dev@25.89.104.232:~/Downloads
