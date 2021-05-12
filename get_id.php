57bc408cc66079eb4bda2f9afedfd7587c2496c1
jja42 <jja2@njit.edu>
2021-05-12 18:34:46 -0400

    Hashing In Game Login

--- get_id.php
+++ get_id.php
@@ -1,6 +1,7 @@
 <?php
 $username = $_POST["username"];
 $password = $_POST["password"];
-$output = shell_exec("php rabbitmqphp_example/ID_Request.php $username $password");
+$hashed_password = password_hash($password, PASSWORD_BCRYPT)
+$output = shell_exec("php rabbitmqphp_example/ID_Request.php $username $hashed_password");
 echo $output;	
 ?>
