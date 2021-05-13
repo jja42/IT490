ba7ae1bba419b9d9e42f0d67ca77d942c7e21b78
jja42 <47000512+jja42@users.noreply.github.com>
2021-05-12 20:32:37 -0400

    Reverting Change

--- get_id.php
+++ get_id.php
@@ -1,7 +1,6 @@
 <?php
 $username = $_POST["username"];
 $password = $_POST["password"];
-$hashed_password = password_hash($password, PASSWORD_BCRYPT)
-$output = shell_exec("php rabbitmqphp_example/ID_Request.php $username $hashed_password");
+$output = shell_exec("php rabbitmqphp_example/ID_Request.php $username $password");
 echo $output;	
 ?>
